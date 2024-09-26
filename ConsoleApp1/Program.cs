15c7307ef8beb6bb5006ed5e8fd55a5a7a4207ce
23-0085c <23-0085c@CCL01-05>
2024-09-26 16:05:28 +0800

    2ND PROGRESS (WITH BOARD CHANGE)

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -11,21 +12,27 @@ namespace ConsoleApp1
     internal class Program
     {
         static Random rnd = new Random();
-        static List<int> list = new List<int>();
+        static List<int> defaultValues = new List<int>();
+        static List<int> recordedValues = new List<int>();
         static int[] defaultArray = new int[15];
+
         static void Main(string[] args)
         {
+            int choice = 0;
+            GenerateValues();
+
             while (true)
             {
+                
                 Console.Clear();
-                DisplayBoard(defaultArray, 5);
-
+                DisplayBoard(defaultArray, 5, choice);
+                
                 Console.WriteLine("[1] Draw \t[2] End");
                 Console.Write("\nChoose an option: ");
 
-                if (OptionSelect())
+                if (OptionSelect(out choice))
                 {
-                    
+                    Operation(choice);
                     
                 }
                 else
@@ -38,46 +45,134 @@ namespace ConsoleApp1
             
         }
 
-        static bool OptionSelect()
+        static void GenerateValues()
+        {
+            for (int i = 1; i < 76; i++)
+            {
+                defaultValues.Add(i);
+            }
+        }
+        // For Default List Generation
+
+        static bool OptionSelect(out int choice)
         {        
            int userInput = int.Parse(Console.ReadLine());
 
            if (userInput == 1 || userInput == 2)
            {
+                choice = userInput;
                 return true;
            }
            else
            {
                 Console.WriteLine("\nAn error has occurred, please try inputting a valid value");
                 Console.ReadKey();
+                choice = 0;
                 return false;
            }
         }
+        /// <summary>
+        /// For user input
+        /// </summary>
+        /// <param name="choice"></param>
 
-        static void DisplayBoard(int[] array, int num) // Display
+        static void Operation(int choice)
+        {
+            if (choice == 1)
+            {
+                Draw();
+            }
+            else
+            {
+                End();
+            }
+
+        }
+
+        static void Draw()
+        {
+            while (true)
+            {
+                int randomNum = rnd.Next(1, 76);
+
+                if (!recordedValues.Contains(randomNum))
+                {
+                    recordedValues.Add(randomNum);
+                    Console.WriteLine($"Player has drew {randomNum}");
+                    Console.ReadKey();
+                    break;
+                }
+            }
+            
+        }
+
+        static void End()
+        {
+
+        }
+
+
+
+
+
+
+        static void DisplayBoard(int[] array, int num, int choice)
         {
             int[] values = array;
             string bingoDisplay = "BINGO";
             int count = 0;
+            int tempChoice = choice;
 
-            while (num > 0)
+            if (tempChoice == 0 || tempChoice == 2)
             {
-                values = GenerateValues(array, num);
-
-                Console.Write(bingoDisplay[count] + "\t-\t");
-
-                foreach (int i in values)
+                while (num > 0)
                 {
-                    Console.Write(i + "\t");
+                    values = GenerateValues(array, num);
+
+                    Console.Write(bingoDisplay[count] + "\t-\t");
+
+                    foreach (int i in values)
+                    {
+                        Console.Write(i + "\t");
+                    }
+                    Console.WriteLine("\n");
+                    num--;
+                    count++;
                 }
                 Console.WriteLine("\n");
-                num--;
-                count++;
             }
-            Console.WriteLine("\n");
-        }
+            else if (tempChoice == 1)
+            {
+                while (num > 0)
+                {
+                    values = GenerateValues(array, num);
 
-        static int[] GenerateValues(int[] array, int num) // Generating Values
+                    foreach (int i in values)
+                    {
+                        if (recordedValues.Contains(i))
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write(i + "\t");
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.Write(i + "\t");
+                        }
+                    }
+                    Console.WriteLine("\n");
+                    num--;
+                }
+            }     
+        }
+        /// <summary>
+        /// Displaying Values for Default Board
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="num"></param>
+        /// <returns></returns>
+
+        static int[] GenerateValues(int[] array, int num) 
         {
             for (int i = 0; i < array.Length; i++)
             {
@@ -88,7 +183,20 @@ namespace ConsoleApp1
             }
             return array;
         }
+        /// <summary>
+        /// Generating Values for Default Board
+        /// </summary>
+        /// 
 
+        static void GenrateValues()
+        {
+            for (int i = 1; i < 76; i++)
+            {
+                defaultValues.Add(i);
+            }
+        }
+
+        
         
     }
 }
